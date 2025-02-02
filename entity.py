743a0a0dcf1dc334f98c896e58a51d3cf6618e47
MahshasdRoustapour <mahshadroustapour1@gmail.com>
2025-02-02 18:00:46 +0330

    feat: def init

--- entity.py
+++ entity.py
@@ -43,30 +43,14 @@ class Teacher:
             print("No teachers found.")
             return []
 
+
 class Student:
-    def __init__(self, name, email, class_id):
+    def init(self, name, email, class_id):
         self.name = name
         self.email = email
         self.class_id = class_id
-    
-    def add_student(self, db):
-        """Add student to database with class_id validation"""
-        pass
-    
-    @staticmethod
-    def remove_student(db, student_id):
-        """Remove student from database"""
-        pass
-    
-    @staticmethod
-    def update_student(db, student_id, new_name=None, new_email=None, new_class_id=None):
-        """Edit student details"""
-        pass
-    
-    @staticmethod
-    def view_students(db):
-        """Fetch all students"""
-        pass
+
+   
 
 
 class Course:
